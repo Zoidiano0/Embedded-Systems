1110b5334ea1d5a2f212582798be9d3e41f828bb
Santiago Estevez <santiestvez@gmail.com>
2018-10-28 18:50:49 -0500

    Comentado

--- Tareas 29 Octubre/Palabra_Serial/Palabra_Serial.ino
+++ Tareas 29 Octubre/Palabra_Serial/Palabra_Serial.ino
@@ -1,8 +1,14 @@
 //Librerias
 #include <LiquidCrystal.h> 
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+
+
 void setup() {
+
+  //Serial
   Serial.begin(9600);
+
+  //LCD
   lcd.begin(20,4);
   lcd.setCursor(0,0);
   lcd.print("Palabra 8 Char");
@@ -11,7 +17,7 @@ void setup() {
 
 void loop() {
       
-        String string = Serial.readStringUntil(':');
+        String string = Serial.readStringUntil(':');//Lee entrada serial hasta caracter especificado
         if(string != ""){
             //here you could check the servo number
             String pos = Serial.readStringUntil('&');
@@ -21,22 +27,23 @@ void loop() {
             Serial.println(string);
              Serial.println(string.length());
              lcd.clear();
-             char vector[string.length()] {};
+             char vector[string.length()] {};   // crea vector del tamanio de palabra recoletada
             for(int i=0; i<=(string.length()-1); i++) {
-             vector[i]=string.charAt(i);
+             vector[i]=string.charAt(i);              // asigna las letras de la palabara dentro del vector
+             
                 Serial.println(vector[i]);
         }
 
          for(int i=0; i<=(string.length()-1); i++) {
              vector[i]=string.charAt(i);
-             lcd.setCursor(i,0);
+             lcd.setCursor(i,0);                              // imprime letras de vectro en lcd en orden ascendente
               lcd.print(vector[i]);
               delay(200);              
         }
 
 
            for(int i=(string.length()-1); i>=0; i--) {
-             
+                                                                     // Borra letras de vectro en lcd en orden ascendente
              lcd.setCursor(i,0);
               lcd.print(" ");
               delay(200);              
@@ -46,7 +53,7 @@ void loop() {
 
         for(int i=(string.length()-1); i>=0; i--) {
              vector[i]=string.charAt(i);
-             lcd.setCursor(i,0);
+             lcd.setCursor(i,0);                               // imprime letras de vectro en lcd en orden decendente
               lcd.print(vector[i]);
               delay(200);              
         }
