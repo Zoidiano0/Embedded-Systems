5af07f9444c3d3f27b6e18377db69599a9dc415d
Santiago Estevez <santiestvez@gmail.com>
2018-11-09 09:35:29 -0500

    Knm

--- Tareas 9 Nov/KNN/KNN.ino
+++ Tareas 9 Nov/KNN/KNN.ino
@@ -1,37 +1,79 @@
 #include"train.h"
 #include"test.h"
-
-
+int respuestas[100];
+int posiciones[100];
 int col=0;
 int row=0;
 float distancia;
 float dist_menor=1000;
 int tcont=0;
 
+int contres=0;
+
+int kn=3;
+
 int contt1=0;
 int contt2=0;
 int contt3=0;
 int tval=15;
-
+int runtime=0;
 void setup() {
   // put your setup code here, to run once:
  Serial.begin(9600);
 }
 
 void loop() {
+  while(runtime==0){
 
-  for(;row<120;row++){ // contador de filas (for lento)
+ knm(5);
+
+runtime=1;
+  }
+}
+
+
+void knm(int k){
+
+   for(;row<120;row++){ // contador de filas (for lento)
        distancia= sqrt( pow((matriz[row][0]-prueba[tval][0]),2)+
                         pow((matriz[row][1]-prueba[tval][1]),2)+
                         pow((matriz[row][2]-prueba[tval][2]),2)+
                         pow((matriz[row][3]-prueba[tval][3]),2));
       if(distancia<dist_menor){ 
+        
        dist_menor=distancia;   
        Serial.println(dist_menor);
        tcont=matriz[row][4];
        Serial.println(tcont);
+
        
-         switch(tcont){
+       respuestas[contres]=distancia;
+       posiciones[contres]=matriz[row][4];
+        contres++;
+
+        
+Serial.println("Valoes guardado");
+Serial.println(contres);
+
+  
+       
+     }  
+
+         
+      
+  }
+
+
+
+
+    for(int h=(contres-1);h>=(contres-k);h--){
+  Serial.println("Valoes de h");
+      Serial.println(h);
+       Serial.println(respuestas[h]);
+        Serial.println(posiciones[h]);
+        
+         switch(posiciones[h]){
+          
           case 1:
                  Serial.println("Setosa");
                  contt1++;
@@ -46,21 +88,18 @@ void loop() {
                  break;
         
         }
-       
-     }             
-      
-  }
+     
+    }
 
-/*
- * Lectura de datos
-  for(;row<120;row++){ // contador de filas (for lento)
-    for(;col<5;col++){ // contador de col (for rapido)
-      Serial.print(matriz[row][col]);  // imprime dato de matriz
-      Serial.print(',');            // separador de dato
-      delay(25);
-      }
-      col=0;        // reinicio de columna
-      Serial.println(' ');   // caracter en blanco de avance de linea
-  }
-  */    
+
+
+
+
+
+
+
+
+  
+
+ 
 }
