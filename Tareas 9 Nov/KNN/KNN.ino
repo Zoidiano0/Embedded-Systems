f3e92d4938e9a6c67e9c5b1aa4bd595f9394c972
Santiago Estevez <santiestvez@gmail.com>
2018-11-09 08:24:29 -0500

    Corrected KNN

--- Tareas 9 Nov/KNN/KNN.ino
+++ Tareas 9 Nov/KNN/KNN.ino
@@ -1,92 +1,37 @@
-#include <ArduinoSort.h>
-
-#include"test.h"
 #include"train.h"
+#include"test.h"
+
+
 int col=0;
 int row=0;
 float distancia;
-float distancia_menor=1000;
-
-//Valor de Prueba a determinar
-int valt=80;
-byte valores[120];
-int tipos[3];
+float dist_menor=1000;
 int tcont=0;
+
 int contt1=0;
 int contt2=0;
 int contt3=0;
+int tval=15;
 
-int runtime=0;
 void setup() {
-  
-  Serial.begin(115200);
-
-//  for(;row<120;row++){
-//    for(;col<5;col++){
-//      Serial.print(matriz[row][col]);
-//      Serial.print(',');
-//      
-//      }
-//      col=0;
-//      Serial.println(' ');
-//    }
-
- }
-
-
-
-
-
-
-
-
-
-
-
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+}
 
 void loop() {
-while(runtime==0){
-for(;row<120;row++){
-   
-      distancia=sqrt( pow((matriz[row][0]-prueba[valt][0]),2)+pow((matriz[row][1]-prueba[valt][1]),2)+ pow((matriz[row][2]-prueba[valt][2]),2)+pow((matriz[row][3]-prueba[valt][3]),2)+pow((matriz[row][4]-prueba[valt][4]),2));
-          
-          
 
-valores[row]=distancia;
-
-         
-        
-        
-        Serial.println("");
-        Serial.print("int: ");
-        Serial.print(distancia);
-         Serial.print("byte: ");
-        Serial.print( valores[row]);
-
-   
-      }
-
-   for(;col<120;col++){
- 
-      
-  
-  Serial.println(valores[col]);
-}
-     
-  /*printArray(valores, sizeof(valores));
- isort(valores, sizeof(valores));
- printArray(valores, sizeof(valores));*/
+  for(;row<120;row++){ // contador de filas (for lento)
+       distancia= sqrt( pow((matriz[row][0]-prueba[tval][0]),2)+
+                        pow((matriz[row][1]-prueba[tval][1]),2)+
+                        pow((matriz[row][2]-prueba[tval][2]),2)+
+                        pow((matriz[row][3]-prueba[tval][3]),2));
+      if(distancia<dist_menor){ 
+       dist_menor=distancia;   
+       Serial.println(dist_menor);
+       tcont=matriz[row][4];
+       Serial.println(tcont);
        
-runtime=1;
-  }
-}
-
-
-
-
-
-void tipo(){
-   switch(tcont){
+         switch(tcont){
           case 1:
                  Serial.println("Setosa");
                  contt1++;
@@ -99,5 +44,23 @@ void tipo(){
                 Serial.println("Virginica");
                 contt3++;
                  break;
+        
         }
+       
+     }             
+      
+  }
+
+/*
+ * Lectura de datos
+  for(;row<120;row++){ // contador de filas (for lento)
+    for(;col<5;col++){ // contador de col (for rapido)
+      Serial.print(matriz[row][col]);  // imprime dato de matriz
+      Serial.print(',');            // separador de dato
+      delay(25);
+      }
+      col=0;        // reinicio de columna
+      Serial.println(' ');   // caracter en blanco de avance de linea
+  }
+  */    
 }
